cf4667f5f5276889bbf0332af70acfc5b772e459
jleonmen5 <71198135+jleonmen5@users.noreply.github.com>
2022-07-14 10:07:14 -0400

    adding a path to test on mac

--- irrs_translator.py
+++ irrs_translator.py
@@ -3,17 +3,19 @@ from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
 from openpyxl import load_workbook
 
+path_mac_jm = "/Users/javier/Documents/GitHub/irrs_tool_py/example.xlsx"
+path_mac_jm2 = "/Users/javier/Documents/GitHub/irrs_tool_py/example_changed.xlsx"
 path = "C:\\Users\\aymee.rodriguez\\OneDrive - Exactech, Inc\\Projects\\irrs_tool_py\\example.xlsx"
 path2 = "C:\\Users\\aymee.rodriguez\\OneDrive - Exactech, Inc\\Projects\\irrs_tool_py\\example_changed.xlsx"
 
-wb = load_workbook(path)
+wb = load_workbook(path_mac_jm)
 ws = wb.active
 cell = ws.cell(row = 1, column = 1)
 print(cell.value)
 
 ws['A1'].font = Font(name= 'Y14.5-2009')
 
-wb.save(path2)
+wb.save(path_mac_jm2)
 # TODO:
     # [] add a function to read an excel table with the codes
     #   translation
