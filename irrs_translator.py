64bd7598c01c51875354feda4ee6449249e2a8f8
jleonmen5 <71198135+jleonmen5@users.noreply.github.com>
2022-07-15 17:13:07 -0400

    first replacement prototype

--- irrs_translator.py
+++ irrs_translator.py
@@ -11,7 +11,14 @@ path2 = "C:\\Users\\aymee.rodriguez\\OneDrive - Exactech, Inc\\Projects\\irrs_to
 wb = load_workbook(path_mac_jm)
 ws = wb.active
 cell = ws.cell(row = 1, column = 1)
-print(cell.value)
+wrong_symbols = str(cell.value)
+right_symbols = wrong_symbols.replace("|", "{", 1)
+right_symbols = right_symbols[::-1].replace("|", "}", 1)
+right_symbols = right_symbols[::-1]
+right_symbols = right_symbols.replace("⌖", "¿~", 1)
+right_symbols = right_symbols.replace("Ⓜ", "Ì~", 1)
+print(right_symbols)
+ws['A1'] = right_symbols
 ws['A1'].font = Font(name= 'Y14.5-2009')
 
 wb.save(path_mac_jm2)
