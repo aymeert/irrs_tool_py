0a68e2174e71e071245409ce2c445c5b9a0e6d04
jleonmen5 <71198135+jleonmen5@users.noreply.github.com>
2022-07-18 15:55:52 -0400

    translating only cells that can be identified

--- irrs_translator.py
+++ irrs_translator.py
@@ -13,8 +13,12 @@ path = "C:\\Users\\aymee.rodriguez\\OneDrive - Exactech, Inc\\Projects\\irrs_too
 path2 = "C:\\Users\\aymee.rodriguez\\OneDrive - Exactech, Inc\\Projects\\irrs_tool_py\\example_changed.xlsx"
 
 path_full_irss = "C:\\Users\\aymee.rodriguez\\OneDrive - Exactech, Inc\\Projects\\irrs_tool_py\\QC322-110-00 Rev A 2022-07-15-15-22-38.xlsx"
+path_full_irss_mac = "/Users/javier/Documents/GitHub/irrs_tool_py/QC322-110-00 Rev A 2022-07-15-15-22-38.xlsx"
 
-
+def open_workbook(path_to_workbook):
+    workbook = load_workbook(path_to_workbook)
+    worksheet = workbook["Final Or Supplier Manufactured"]
+    return workbook, worksheet
 
 def find_bp_specification(worksheet):
     start_cell = "BP Specification"
@@ -27,40 +31,51 @@ def find_bp_specification(worksheet):
 
 def iterate_through_column(worksheet):
     start_row, start_col = find_bp_specification(worksheet)
-    for row in range(start_row, worksheet.max_row):
-        print(worksheet.cell(row,start_col).value)
+    for row in range(start_row + 1, worksheet.max_row):
+        cell = worksheet.cell(row,start_col)
+        if cell.value is None:
+            break
+        cell.font = Font(name= 'Y14.5-2009')
+        translated_symbols = translate_by_cell_type(cell)
+        cell.value = translated_symbols
+    return worksheet
 
+def frame_simple_cell(cell):
+    wrong_symbols = str(cell.value)
+    wrong_symbols = str(cell.value)
+    right_symbols = wrong_symbols.replace("|", "{", 1)
+    right_symbols = right_symbols[::-1].replace("|", "}", 1)
+    right_symbols = right_symbols[::-1]
+    right_symbols = right_symbols.replace("⌖", "¿~", 1)
+    right_symbols = right_symbols.replace("Ⓜ", "Ì~", 1)
+    cell.font = Font(name= 'Y14.5-2009')
+    first_half = right_symbols.split('{')[0]
+    second_half = right_symbols.split('{')[1]
+    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz."
+    final_symbols = ""
+    for letter in second_half:
+        if letter in alphabet:
+            letter = letter + "`"
+        final_symbols = final_symbols + letter
+    final_symbols = first_half+'{'+final_symbols
+    return final_symbols
 
-wb = load_workbook(path_full_irss)
-ws = wb.active
+def translate_by_cell_type(cell):
+    cell_content = cell.value
+    if is_simple_frame(cell_content):
+        translated_cell = frame_simple_cell(cell)
+    else: translated_cell = cell_content
+    return translated_cell
 
-iterate_through_column(ws)
+def is_simple_frame(cell_content):
+    if cell_content.count("|") >= 2:
+        return True
+    else: return False
 
-cell = ws.cell(row = 1, column = 1)
-wrong_symbols = str(cell.value)
-right_symbols = wrong_symbols.replace("|", "{", 1)
-right_symbols = right_symbols[::-1].replace("|", "}", 1)
-right_symbols = right_symbols[::-1]
-right_symbols = right_symbols.replace("⌖", "¿~", 1)
-right_symbols = right_symbols.replace("Ⓜ", "Ì~", 1)
-# print(right_symbols)
-# ws['A1'] = right_symbols
-ws['A1'].font = Font(name= 'Y14.5-2009')
+workbook, worksheet  = open_workbook(path_full_irss_mac)
+translated_worksheet = iterate_through_column(worksheet)
+workbook.save(path_mac_jm2)
 
-first_half = right_symbols.split('{')[0]
-second_half = right_symbols.split('{')[1]
-
-alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz."
-final_symbols = ""
-for letter in second_half:
-    if letter in alphabet:
-        letter = letter + "`"
-    final_symbols = final_symbols + letter
-final_symbols = first_half+'{'+final_symbols
-# print(final_symbols)
-
-ws['A1'] = final_symbols
-wb.save(path2)
 # TODO:
     # [] add a function to read an excel table with the codes
     #   translation
