a97ee8566e4a24be13a9bc76b3bb585789aa193f
jleonmen5 <71198135+jleonmen5@users.noreply.github.com>
2022-07-20 09:26:28 -0400

    adding doctrings and todo

--- irrs_translator.py
+++ irrs_translator.py
@@ -14,28 +14,14 @@ path_full_irss_ar = "C:\\Users\\aymee.rodriguez\\OneDrive - Exactech, Inc\\Proje
 path_to_translation_table = "/Users/javier/Documents/GitHub/irrs_tool_py/translation_table.xlsx"
 
 def open_workbook(path_to_workbook):
-    """brief description
-    
-    Args:
-        Object in
-    
-    Returns:
-        Object out
-    """
+    """Opens the IRRS to be translated"""
     workbook = load_workbook(path_to_workbook)
     worksheet = workbook["Final Or Supplier Manufactured"]
     return workbook, worksheet
 
 
 def find_bp_specification(worksheet):
-    """brief description
-    
-    Args:
-        Object in
-    
-    Returns:
-        Object out
-    """
+    """Finds the column header to be trasnlated"""
     start_cell = "BP Specification"
     for col in range(worksheet.min_column, worksheet.max_column):
         for row in range(worksheet.min_row, worksheet.max_row):
@@ -46,114 +32,87 @@ def find_bp_specification(worksheet):
 
 
 def iterate_through_column(worksheet):
-    """brief description
-    
-    Args:
-        Object in
-    
-    Returns:
-        Object out
-    """
+    """Acesses cell by cell in the column to be translated"""
     start_row, start_col = find_bp_specification(worksheet)
     for row in range(start_row + 1, worksheet.max_row):
         cell = worksheet.cell(row,start_col)
         if cell.value is None:
             break
-        cell.font = Font(name= 'Y14.5-2009', size=12)
+        cell.font = Font(name= 'Y14.5-2009', size=13)
         translated_symbols = translate_by_cell_type(cell)
         cell.value = translated_symbols
     return worksheet
 
 
 def frame_simple_cell(cell):
-    """brief description
-    
-    Args:
-        Object in
-    
-    Returns:
-        Object out
-    """
-   # This should be a function
-    wrong_symbols = str(cell.value)
-    right_symbols = wrong_symbols.replace("|", "{", 1)
-    right_symbols = right_symbols[::-1].replace("|", "}", 1)
-    right_symbols = right_symbols[::-1]
-    right_symbols = right_symbols.replace("⌖", "¿~", 1)
-    right_symbols = right_symbols.replace("Ⓜ", "Ì~", 1)
-    
-    # This should be another function
-    first_half = right_symbols.split('{')[0]
-    second_half = right_symbols.split('{')[1]
+    """Adds framing to cells that require a single GD&T frame"""
+    translated_symbols = translate_gdt_symbols(cell)
+    translated_symbols = translated_symbols.replace("|", "{", 1)
+    translated_symbols = translated_symbols[::-1].replace("|", "}", 1)
+    translated_symbols = translated_symbols[::-1]
+    before_braket = translated_symbols.split('{')[0]
+    after_braket = translated_symbols.split('{')[1]
     alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz."
-    final_symbols = ""
-    for letter in second_half:
+    complete_translation = ""
+    for letter in after_braket:
         if letter in alphabet:
-            letter = letter + "`"
-        final_symbols = final_symbols + letter
-    final_symbols = first_half+'{'+final_symbols
-    return final_symbols
+            letter = letter + "_"
+        complete_translation = complete_translation + letter
+    complete_translation = before_braket + '{' + complete_translation
+    return complete_translation
 
 
 def read_translation_table(path_to_translation_table):
+    """Reads the translation table containing the GD&T symbols"""
     workbook = load_workbook(path_to_translation_table)
     translation_table = workbook["Translations"]
     return translation_table
 
 
 def translate_gdt_symbols(cell):
-    #str(cell.value)
-    right_symbols = "2X |⌖|.009Ⓜ|B|"
+    """Translates the GD&T symbols from the translation table"""
+    translated_symbols = str(cell.value)
     translation_table = read_translation_table(path_to_translation_table)
     for row in range(translation_table.min_row + 1, translation_table.max_row):
         correct_symbol = str(translation_table.cell(row,2).value)
         incorrect_symbol = translation_table.cell(row,3).value #not reading it as a string initially
         if incorrect_symbol:
-            right_symbols = right_symbols.replace(str(incorrect_symbol), correct_symbol + "~", 1)
-    print(right_symbols)
-    return
+            translated_symbols = translated_symbols.replace(str(incorrect_symbol), correct_symbol + "~", 1)
+    return translated_symbols
 
 
 def translate_by_cell_type(cell):
-    """brief description
-    
-    Args:
-        Object in
-    
-    Returns:
-        Object out
-    """
+    """Identifies the kind of cell to use the appropriate translation method"""
     cell_content = cell.value
     if is_simple_frame(cell_content):
         translated_cell = frame_simple_cell(cell)
     else: translated_cell = cell_content
     return translated_cell
 
+
 def is_simple_frame(cell_content):
-    """identifies if a cell's content is a simple GDT frame
-    
-    Args:
-        String: the contents of a cell wiht a gdt tag to be identified
-    
-    Returns:
-        Boolean: wether the string is a simple frame or not
-    """
+    """Identifies if a cell's content is a simple GD&T frame"""
     if cell_content.count("|") >= 2:
         return True
     else: return False
 
-workbook, worksheet  = open_workbook(path_full_irss)                   # exporting into another excel file
+workbook, worksheet  = open_workbook(path_full_irss_mac)
 translated_worksheet = iterate_through_column(worksheet)
+workbook.save(path_mac_jm2)
 
 # TODO:
-    # [] add a function to read an excel table with the codes
-    #   translation
-    # [] modify the processIRRS function to replace the symbols based
-    #   on the translation table
-    # [X] add a function to read each IRRS to be translated
-    # [X] add a function to export each translated IRRS
+"""
+    [] add a function to read an excel table with the codes
+    translation
+    [] modify the processIRRS function to replace the symbols based
+    on the translation table
+    [X] add a function to read each IRRS to be translated
+    [X] add a function to export each translated IRRS
+    [] add 5 cases:
+        [X] simple frame
+        [] double frame
+        [] not framed but translated
+        [] Ra
+        [X] nothing needs to happen
     # [] create a user interface
-#testing
-translate_gdt_symbols(worksheet)
-
-# workbook.save(path_mac_jm2)
+"""
