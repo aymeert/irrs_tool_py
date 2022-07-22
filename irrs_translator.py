5f5ca60b62b29d72acdeccaa3c272b0abb8438af
jleonmen5 <71198135+jleonmen5@users.noreply.github.com>
2022-07-21 22:38:11 -0400

    defined add frames to sting function

--- irrs_translator.py
+++ irrs_translator.py
@@ -11,7 +11,7 @@ path_full_irss = "C:\\Users\\aymee.rodriguez\\OneDrive - Exactech, Inc\\Projects
 path_full_irss_mac = "/Users/javier/Documents/GitHub/irrs_tool_py/QC322-110-00 Rev A 2022-07-15-15-22-38.xlsx"
 path_full_irss_ar = "C:\\Users\\aymee.rodriguez\\OneDrive - Exactech, Inc\\Projects\\irrs_tool_py\\QC322-110-00 Rev A changed 2022-07-15-15-22-38.xlsx"
 
-path_to_translation_table_mac = "/Users/javier/Documents/GitHub/irrs_tool_py/translation_table.xlsx"
+path_to_translation_table = "/Users/javier/Documents/GitHub/irrs_tool_py/translation_table.xlsx"
 path_to_translation_table_win = "C:\\Users\\aymee.rodriguez\\OneDrive - Exactech, Inc\\Projects\\irrs_tool_py\\translation_table.xlsx"
 
 def open_workbook(path_to_workbook):
@@ -45,55 +45,6 @@ def iterate_through_column(worksheet):
     return worksheet
 
 
-def frame_simple_cell(cell):
-    """Adds framing to cells that require a single GD&T frame"""
-    translated_symbols = translate_gdt_symbols(cell)
-    translated_symbols = translated_symbols.replace("|", "{", 1)
-    translated_symbols = translated_symbols[::-1].replace("|", "}", 1)
-    translated_symbols = translated_symbols[::-1]
-    before_braket = translated_symbols.split('{')[0]
-    after_braket = translated_symbols.split('{')[1]
-    #need to brake this off as it's own function
-    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz."
-    complete_translation = ""
-    for letter in after_braket:
-        if letter in alphabet:
-            letter = letter + "_"
-        complete_translation = complete_translation + letter
-    complete_translation = before_braket + '{' + complete_translation
-    return complete_translation
-
-
-def add_frames_to_characters():
-    """Adds frames to individual alpha numeric characters and special GD&T symbols"""
-    
-
-
-def get_list_of_gdt_symbols():
-    """Gets a list of all the GD&T symbols from the translation table"""
-    translation_table = read_translation_table(path_to_translation_table_win) # change the path depending on the OS
-
-
-def read_translation_table(path_to_translation_table):
-    """Reads the translation table containing the GD&T symbols"""
-    workbook = load_workbook(path_to_translation_table)
-    translation_table = workbook["Translations"]
-    return translation_table
-
-
-def translate_gdt_symbols(cell):
-    """Translates the GD&T symbols from the translation table"""
-    translated_symbols = str(cell.value)
-    translation_table = read_translation_table(path_to_translation_table_win) # change the path depending on the OS
-    for row in range(translation_table.min_row + 1, translation_table.max_row):
-        correct_symbol = str(translation_table.cell(row,2).value)
-        incorrect_symbol = translation_table.cell(row,3).value #not reading it as a string initially
-        if incorrect_symbol:
-            translated_symbols = translated_symbols.replace(str(incorrect_symbol), correct_symbol + "~", 1)
-            # need to remove adding a ~, this should be reserved for the simple frame function
-    return translated_symbols
-
-
 def translate_by_cell_type(cell):
     """Identifies the kind of cell to use the appropriate translation method"""
     cell_content = cell.value
@@ -109,9 +60,67 @@ def is_simple_frame(cell_content):
         return True
     else: return False
 
-workbook, worksheet  = open_workbook(path_full_irss)
+
+def frame_simple_cell(cell):
+    """Adds framing to cells that require a single GD&T frame"""
+    translated_symbols = translate_gdt_symbols(cell)
+    translated_symbols = translated_symbols.replace("|", "{", 1)
+    translated_symbols = translated_symbols[::-1].replace("|", "}", 1)
+    translated_symbols = translated_symbols[::-1]
+    before_braket = translated_symbols.split('{')[0]
+    after_braket = translated_symbols.split('{')[1]
+    framed_characters = add_frames_to_characters(after_braket)
+    complete_translation = before_braket + '{' + framed_characters
+    return complete_translation
+
+
+def translate_gdt_symbols(cell):
+    """Translates the GD&T symbols from the translation table"""
+    translated_symbols = str(cell.value)
+    translation_table = read_translation_table(path_to_translation_table) # change the path depending on the OS
+    for row in range(translation_table.min_row + 1, translation_table.max_row):
+        correct_symbol = str(translation_table.cell(row,2).value)
+        incorrect_symbol = translation_table.cell(row,3).value #not reading it as a string initially
+        if incorrect_symbol:
+            translated_symbols = translated_symbols.replace(str(incorrect_symbol), correct_symbol, 1)
+    return translated_symbols
+
+
+def read_translation_table(path_to_translation_table):
+    """Reads the translation table containing the GD&T symbols"""
+    workbook = load_workbook(path_to_translation_table)
+    translation_table = workbook["Translations"]
+    return translation_table
+
+
+def add_frames_to_characters(characters_to_frame):
+    """Adds frames to individual alpha numeric characters and special GD&T symbols"""
+    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz."
+    gdt_symbols = get_list_of_gdt_symbols(path_to_translation_table)
+    framed_characters = ""
+    for character in characters_to_frame:
+        if character in alphabet:
+            character = character + "_"
+        if character in gdt_symbols:
+            character = character + "~"
+        framed_characters = framed_characters + character
+    return framed_characters
+
+
+def get_list_of_gdt_symbols(path_to_translation_table):
+    """Gets a list of all the GD&T symbols from the translation table"""
+    translation_table = read_translation_table(path_to_translation_table) # change the path depending on the OS
+    gdt_symbols = ""
+    for row in range(translation_table.min_row + 1, translation_table.max_row):
+        incorrect_symbol = translation_table.cell(row,3).value #not reading it as a string initially
+        if incorrect_symbol:
+            gdt_symbols = gdt_symbols + str(translation_table.cell(row,2).value)
+    return gdt_symbols
+
+
+workbook, worksheet  = open_workbook(path_full_irss_mac)
 translated_worksheet = iterate_through_column(worksheet)
-workbook.save(path2)
+workbook.save(path_mac_jm2)
 
 # TODO:
 """
@@ -121,6 +130,7 @@ workbook.save(path2)
     on the translation table
     [X] add a function to read each IRRS to be translated
     [X] add a function to export each translated IRRS
+    [] replace all string concatenation with ''.join()
     [] add a function to create the exported file with the same name as orignal and in the same folder
     [] add a function to get the active directory and use the translation table in that directory
     [] add 5 cases:
